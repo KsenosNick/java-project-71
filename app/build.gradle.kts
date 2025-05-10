1e14624edccf4282ae81f51eee35dd1c183403d0
Николай Малютин <xenos-nick@mail.ru>
2025-05-10 19:10:08 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.52.0"
+    application
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
