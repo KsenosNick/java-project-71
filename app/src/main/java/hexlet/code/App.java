82b87e7fc5b3ec3694567c9b756ecc8722de79b7
Николай Малютин <xenos-nick@mail.ru>
2025-05-11 18:50:28 +0300

    docs: Configure help output

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Hello, World!");
     }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+
 }
