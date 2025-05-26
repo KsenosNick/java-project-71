4dfb03ccf890d0efff19970960bc3de7d1ca8ce0
Николай Малютин <xenos-nick@mail.ru>
2025-05-26 18:57:15 +0300

    feat: Add parameters and options to the cli utility

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,12 +5,35 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.util.concurrent.Callable;
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App {
+public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private String filePath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private String filePath2;
+
+    @Option(
+            names = {"-f", "--format"},
+            paramLabel = "format",
+            description = "output format [default: stylish]",
+            defaultValue = "stylish"
+    )
+    private String format;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        String difference = Differ.generate(filePath1, filePath2, format);
+        System.out.println(difference);
+        return 0;
     }
 }
