45064aa65ab5c65dd6c97ba2870f4be34381ad10
Николай Малютин <xenos-nick@mail.ru>
2025-06-12 11:29:38 +0300

    test: Add a test for the generate method of the Differ class

---
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -0,0 +1,26 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DifferTest {
+
+    @Test
+    void generate() throws IOException {
+        String expectedResult = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
+
+        String actualResult = Differ.generate("../../file1.json", "../../file2.json", "stylish");
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
