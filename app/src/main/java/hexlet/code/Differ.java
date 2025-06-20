6c6a7d9bb5eaa6b9752106e7341fd790b4b534b6
Николай Малютин <xenos-nick@mail.ru>
2025-06-20 19:07:53 +0300

    feat: Add parsing of files in yaml format

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -8,21 +8,30 @@ import static hexlet.code.Comparator.compare;
 import static hexlet.code.Formater.format;
 import static hexlet.code.Parser.parse;
 import static hexlet.code.Reader.read;
+import org.apache.commons.io.FilenameUtils;
 
 public class Differ {
     public static String generate(String filePath1, String filePath2, String format) throws IOException {
-        String fileContent1 = read(filePath1);
-        String fileContent2 = read(filePath2);
+        try {
+            String file1Format = FilenameUtils.getExtension(filePath1).toLowerCase();
+            String file2Format = FilenameUtils.getExtension(filePath2).toLowerCase();
 
-        Map<String, Object> data1 = parse(fileContent1);
-        Map<String, Object> data2 = parse(fileContent2);
+            String fileContent1 = read(filePath1);
+            String fileContent2 = read(filePath2);
 
-        List<Map<String, Object>> difference = compare(data1, data2);
+            Map<String, Object> data1 = parse(fileContent1, file1Format);
+            Map<String, Object> data2 = parse(fileContent2, file2Format);
 
-        String result = format(difference, format);
+            List<Map<String, Object>> difference = compare(data1, data2);
 
-        System.out.println(result);
+            String result = format(difference, format);
 
-        return result;
+            System.out.println(result);
+
+            return result;
+        } catch (IllegalArgumentException exception) {
+            throw new RuntimeException(exception);
+        }
+
     }
 }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -5,11 +5,28 @@ import java.util.Map;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 public class Parser {
-    public static <K, V> Map<K, V> parse(String content) throws IOException {
+    public static <K, V> Map<K, V> parse(String content, String fileFormat) throws IOException {
+        if (fileFormat.equals("json")) {
+            return parseJson(content);
+        } else if (fileFormat.equals("yml")) {
+            return parseYaml(content);
+        } else {
+            throw new IllegalArgumentException("Unsupported file format: " + fileFormat);
+        }
+    }
 
+    private static <K, V> Map<K, V> parseJson(String content) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
+        TypeReference<Map<K, V>> typeRef = new TypeReference<>() { };
+
+        return mapper.readValue(content, typeRef);
+    }
+
+    private static <K, V> Map<K, V> parseYaml(String content) throws IOException {
+        ObjectMapper mapper = new YAMLMapper();
         TypeReference<Map<K, V>> typeRef = new TypeReference<>() { };
 
         return mapper.readValue(content, typeRef);
