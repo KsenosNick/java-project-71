2818fdfd71f58e363dc072b4993290a2a9a638da
Николай Малютин <xenos-nick@mail.ru>
2025-06-20 19:00:59 +0300

    test: Add parametric test for json and yaml formats

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,31 +1,62 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.assertj.core.api.StrictAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
 
-    @Test
-    void generate() throws IOException {
-        String expectedResult = "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
-                + "}";
+    public static Path getPath(String fileName) {
+        return Paths.get("./src/test/resources/" + fileName).toAbsolutePath().normalize();
+    }
 
-        String filePath1 = Paths.get("./src/test/resources/file1.json").toAbsolutePath().normalize().toString();
-        String filePath2 = Paths.get("./src/test/resources/file2.json").toAbsolutePath().normalize().toString();
+    @ParameterizedTest
+    @ValueSource(strings = {"yml", "json"})
+    void testGenerate(String inputFormat) throws IOException {
+        String file1Name = "file1." + inputFormat;
+        String file2Name = "file2." + inputFormat;
+        String expectedResult = """
+                {
+                  - follow: false
+                    host: hexlet.io
+                  - proxy: 123.234.53.22
+                  - timeout: 50
+                  + timeout: 20
+                  + verbose: true
+                }""";
+
+        String filePath1 = getPath(file1Name).toString();
+        String filePath2 = getPath(file2Name).toString();
         String format = "stylish";
 
         String actualResult = Differ.generate(filePath1, filePath2, format);
 
         assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testGenerateIllegalArgumentException() {
+        String file1Name = "file1.txt";
+        String file2Name = "file2.txt";
+
+        String filePath1 = getPath(file1Name).toString();
+        String filePath2 = getPath(file2Name).toString();
+        String format = "stylish";
+
+        Throwable thrown = catchThrowable(
+                () -> Differ.generate(filePath1, filePath2, format)
+        );
+
+        assertThat(thrown)
+                .isInstanceOf(RuntimeException.class)
+                .hasCauseInstanceOf(IllegalArgumentException.class);
     }
 }
