5c1c3a5dea2a59a798196580ce58bbf5f0a5e723
Николай Малютин <xenos-nick@mail.ru>
2025-06-23 18:49:09 +0300

    refactor: Remove unnecessary nested blocks when processing the status in PlainFormatter

--- app/src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -18,7 +18,7 @@ public class PlainFormatter {
             String formattedValue1 = formatValue(value1);
             String formattedValue2 = formatValue(value2);
             switch (status) {
-                case Status.UPDATE -> {
+                case Status.UPDATE:
                     stringBuilder.append("Property '")
                             .append(key)
                             .append("' was updated. From ")
@@ -26,22 +26,23 @@ public class PlainFormatter {
                             .append(" to ")
                             .append(formattedValue2)
                             .append("\n");
-                }
-                case Status.REMOVED -> {
+                    break;
+                case Status.REMOVED:
                     stringBuilder.append("Property '")
-                            .append(key).append("' was removed")
-                            .append("\n");
-                }
-                case Status.ADDED -> {
+                            .append(key)
+                            .append("' was removed\n");
+                    break;
+                case Status.ADDED:
                     stringBuilder.append("Property '")
                             .append(key)
                             .append("' was added with value: ")
                             .append(formattedValue2)
                             .append("\n");
-                }
-                case Status.UNCHANGED -> {
-                }
-                default -> throw new IllegalStateException("Unexpected value: " + status);
+                    break;
+                case Status.UNCHANGED:
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + status);
             }
         }
 
