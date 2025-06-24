7dd2e0f00135a7cb4a9bda63848d7ea0fd0dbe5f
Николай Малютин <xenos-nick@mail.ru>
2025-06-24 20:20:44 +0300

    feat: Add json formatter

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,19 +1,20 @@
 package hexlet.code;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import hexlet.code.formatters.JsonFormatter;
 import hexlet.code.formatters.PlainFormatter;
 import hexlet.code.formatters.StylishFormatter;
 
 public class Formatter {
-    public static String format(List<Map<String, Object>> difference, String format) {
-        if (format.equals("stylish")) {
-            return StylishFormatter.format(difference);
-        } else if (format.equals("plain")) {
-            return PlainFormatter.format((difference));
-        } else {
-            throw new IllegalArgumentException("Unsupported output format: " + format);
-        }
+    public static String format(List<Map<String, Object>> difference, String format) throws IOException {
+        return switch (format) {
+            case "stylish" -> StylishFormatter.format(difference);
+            case "plain" -> PlainFormatter.format((difference));
+            case "json" -> JsonFormatter.format((difference));
+            default -> throw new IllegalArgumentException("Unsupported output format: " + format);
+        };
     }
 }
