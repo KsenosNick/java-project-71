5c7f135c98e6b95f81a703128657d969e0ccb644
Николай Малютин <xenos-nick@mail.ru>
2025-06-30 18:46:38 +0300

    style: Make App class final

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,7 +9,7 @@ import java.util.concurrent.Callable;
 
 @Command(name = "app", mixinStandardHelpOptions = true, version = "1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable<Integer> {
+public final class App implements Callable<Integer> {
 
     @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
     private String filePath1;
