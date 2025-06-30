b33722a2c5b4c0a4e74ccf583e88de5744a2cc40
Николай Малютин <xenos-nick@mail.ru>
2025-06-30 18:30:20 +0300

    feat: Add the overloaded generate() method to the Differ class
    The generate() method with two arguments for the default output format

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -24,6 +24,21 @@ public class Differ {
         return format(difference, format);
     }
 
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        String file1Format = getDataFormat(filePath1);
+        String file2Format = getDataFormat(filePath2);
+
+        String fileContent1 = read(filePath1);
+        String fileContent2 = read(filePath2);
+
+        Map<String, Object> data1 = parse(fileContent1, file1Format);
+        Map<String, Object> data2 = parse(fileContent2, file2Format);
+
+        List<Map<String, Object>> difference = compare(data1, data2);
+
+        return format(difference, "stylish");
+    }
+
     private static String getDataFormat(String filePath) {
         int index = filePath.lastIndexOf('.');
         return index > 0
