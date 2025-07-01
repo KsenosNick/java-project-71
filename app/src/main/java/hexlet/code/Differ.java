5c3d146daa21d37f79ad6e585e2188de7c23a731
Николай Малютин <xenos-nick@mail.ru>
2025-07-01 19:25:47 +0300

    refactor: Remove duplicate code in the Differ class using the delegation method

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -25,18 +25,7 @@ public class Differ {
     }
 
     public static String generate(String filePath1, String filePath2) throws Exception {
-        String file1Format = getDataFormat(filePath1);
-        String file2Format = getDataFormat(filePath2);
-
-        String fileContent1 = read(filePath1);
-        String fileContent2 = read(filePath2);
-
-        Map<String, Object> data1 = parse(fileContent1, file1Format);
-        Map<String, Object> data2 = parse(fileContent2, file2Format);
-
-        List<Map<String, Object>> difference = compare(data1, data2);
-
-        return format(difference, "stylish");
+        return generate(filePath1, filePath2, "stylish");
     }
 
     private static String getDataFormat(String filePath) {
